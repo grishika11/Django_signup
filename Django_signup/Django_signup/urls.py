321cea4b75479d67321eaa287526d4434e3ea737
Rishika Gupta <grishika11@gmail.com>
2021-03-22 15:00:57 +0530

    ---templates_editing

--- Django_signup/Django_signup/urls.py
+++ Django_signup/Django_signup/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import demo.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",demo.views.Reg,name='reg'),
 ]
--- Django_signup/demo/views.py
+++ Django_signup/demo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Reg(request):
+	return render(request,'reg.html')
